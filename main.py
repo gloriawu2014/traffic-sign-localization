85609fab44d62029bb1a02e37d6791dcd0b9f216
gloriawu2014 <gloriawu2014@gmail.com>
2025-11-13 14:58:45 -0500

    import lisa dataset

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ Script for training a traffic sign detection model.
 
 import torch
 import torchvision
+from lisa import LISA
+
+if __name__ == "__main__":
+    # load in LISA dataset
+    dataset = LISA(root='data', download=True, train=True)
