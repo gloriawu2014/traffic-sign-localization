0742f59d84a8483d469441ddc907e5882b2be261
Chloe Lee 28 <chhlee28@hpc-login2.cm.cluster>
2025-12-11 10:20:36 -0500

    updates

--- newultralytics/test.py
+++ newultralytics/test.py
@@ -2,12 +2,12 @@ from ultralytics import YOLO
 
 # Load a YOLO model
 model = YOLO(
-    "/home/common/neural-safety-net/traffic-sign/traffic-sign-localization/ultralytics/runs/detect/train/weights/best.pt"
+    "/home/chhlee28/traffic/traffic-sign-localization/ultralytics/runs/detect/train2/weights/best.pt"
 )
 
 # Validate on separate data
 model.val(
-    data="/home/chhlee28/traffic/traffic-sign-localization/newultralytics/dfgtest.yaml",
-    project="/home/chhlee28/traffic/results",
-    name="dfg_val",
+    data="/home/chhlee28/traffic/traffic-sign-localization/newultralytics/dfg.yaml",
+    project="/home/chhlee28/traffic/traffic-sign-localization/newultralytics/results2",
+    name="yolo2",
 )
